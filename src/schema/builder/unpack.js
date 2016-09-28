397873f780831ae3007957e5e75316ad6481893a
Dejan Siedle <dejan.siedle@icloud.com>
2016-09-28 09:01:01 +0200

    Update

--- src/schema/builder/unpack.js
+++ src/schema/builder/unpack.js
@@ -7,14 +7,47 @@
  *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
-import {ValidResultPathLense, FaultResultPathLense} from './';
-import R from 'ramda';
-import Either from 'data.either';
+import {
+  AmlError,
+  ValidResultPathLense,
+  FaultResultPathLense,
+  FaultCodeLense,
+  FaultStringLense,
+  FaultLegacyDetailLense,
+  FaultLegacyStringLense,
+  TypeAttributeLense
+} from './';
 
+import {
+  view,
+  isNil,
+  groupBy
+} from 'ramda';
+
+import {
+  Left,
+  Right,
+  Either
+} from 'data.either';
 
 export function unpack(soapMessage: JSON) : Either {
-  const result: JSON = R.view(ValidResultPathLense, soapMessage);
-  return R.isNil(result) ?
-    Either.Left(R.view(FaultResultPathLense, soapMessage)) :
-    Either.Right(result);
+  const result: JSON = view(ValidResultPathLense, soapMessage);
+  return isNil(result) ?
+    Left(view(FaultResultPathLense, soapMessage)) :
+    Right(result);
+}
+
+export const groupTypes = groupBy(item => {
+  return view(TypeAttributeLense, item);
+});
+
+export function getErrorFrom(message: JSON) : AmlError {
+  const result = new AmlError();
+
+  result.faultCode = view(FaultCodeLense, message);
+  result.faultString = view(FaultStringLense, message);
+  result.faultLegacyDetail = view(FaultLegacyDetailLense, message);
+  result.faultLegacyString = view(FaultLegacyStringLense, message);
+
+  return result;
 }
